6977fa940bf18b3c0e6bd92ed639d3c39bbc4103
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-11-02 02:00:50 -0300

    Grids

---
+++ GRID-RESPONSIVE/GRID-4x3/index.js
@@ -0,0 +1,12 @@
+const botaoMenu = document.getElementById("botao_menu");
+const subitens = document.querySelector(".subitens");
+
+botaoMenu.addEventListener("click", function () {
+  if (subitens.style.display === "none" || subitens.style.display === "") {
+    subitens.style.display = "block";
+  } else if (subitens.style.display === "block") {
+    subitens.style.display = "block";
+  } else {
+    subitens.style.display = "none";
+  }
+});
