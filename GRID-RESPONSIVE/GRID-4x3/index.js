52791bffd11e15b1d3ddeaabbbe84efb7dc91ef9
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-11-03 14:07:32 -0300

    Implementação javascript

--- GRID-RESPONSIVE/GRID-4x3/index.js
+++ GRID-RESPONSIVE/GRID-4x3/index.js
@@ -4,8 +4,6 @@ const subitens = document.querySelector(".subitens");
 botaoMenu.addEventListener("click", function () {
   if (subitens.style.display === "none" || subitens.style.display === "") {
     subitens.style.display = "block";
-  } else if (subitens.style.display === "block") {
-    subitens.style.display = "block";
   } else {
     subitens.style.display = "none";
   }
